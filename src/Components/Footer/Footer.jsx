b20473743f3c56e5df5b4e561716803e7f5a90be
Jun <jz7259@g.rit.edu>
2024-09-12 19:48:11 -0400

    testing commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+    return <div className={styles.footer}>
+        <h3>I Love You.</h3>
+    </div>
+}
+
+export default Footer
